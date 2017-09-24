917726d9e6e6c97026b8f677cadc149f9af5a42f
lijoos <lijo.8893@gmail.com>
2017-09-25 04:00:55 +0530

    bug fix
    bug fix

--- angular-src/src/app/home/home.component.ts
+++ angular-src/src/app/home/home.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, Input, Output, EventEmitter, AfterViewInit, ViewChild } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, AfterViewInit } from '@angular/core';
 import { BetTable, WinRate } from "app/model/bet";
 import { BetService } from "app/service/BetService";
-import { BetComponent } from "app/bet/bet.component";
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -12,8 +12,7 @@ export class HomeComponent implements OnInit {
     
  dropdownvalue1 :string="Select Customer"
  dropdownvalue2 :string="Select Customer"
-  @ViewChild(BetComponent)
-  private child: BetComponent;
+
   winRate: WinRate[];
   betsSettled: BetTable[];
   AllBetSettled:BetTable[];
@@ -46,14 +45,12 @@ export class HomeComponent implements OnInit {
      this.setUnusulFlag();
    
   }
+  
  //bet setttled
 onSelected(id)
   {
-      this.child.notifyMe();
-      this.startindex=0;
-      this.endindex=10;
+
       this.dropdownvalue1="Customer "+id;
-   // this.AllBetSettled=this.betsSettled;
     this.betsSettled=BetTable[0];
     
 this.betService.getBetHistory(id)
@@ -63,19 +60,24 @@ this.betService.getBetHistory(id)
   }
   showAllBets()
   {
+       this.betService.getBetHistory()
+            .subscribe(bets => {
+                this.betsSettled = bets;
+                this.AllBetSettled=this.betsSettled;
+               
+            });
       this.dropdownvalue1 ="Select Customer"
-    if(this.AllBetSettled!=null)
+   /* if(this.AllBetSettled!=null)
     {
 this.betsSettled=this.AllBetSettled;
-    }
+    }*/
   }
 
   //bets unsettled
   onSelectedUnsettled(id)
   {
-        this.dropdownvalue2="Customer "+id;
-  //  this.AllBetUnSettled=this.betsUnSettled;
-    this.betsUnSettled=BetTable[0];
+     this.dropdownvalue2="Customer "+id;
+     this.betsUnSettled=BetTable[0];
        
 this.betService.getBetData(id)
             .subscribe(bets => {
@@ -85,11 +87,14 @@ this.betService.getBetData(id)
   }
   showAllBetsUnsettled()
   {
-        this.dropdownvalue2="Select Customer"
-     if(this.AllBetUnSettled!=null)
-    {
-this.betsUnSettled=this.AllBetUnSettled;
-    }
+        this.dropdownvalue2="Select Customer";
+      
+     this.betService.getBetData()
+            .subscribe(bets => {
+                this.betsUnSettled = bets;
+                 this.AllBetUnSettled=this.betsUnSettled;
+                this.setUnusulFlag();
+            });
   }
   //set the unusual win flag fo unsettled customers
   setUnusulFlag()
@@ -105,9 +110,9 @@ this.betsUnSettled=this.AllBetUnSettled;
                 bet.isUnSettled=true;
                 bet.isUnusual=win.winPer>=60?true:false;
                 //set the flag If customers stake is more than 10 times higher than that customer’s average bet in their betting history
-                bet.isHighStake=win.avgStake*10<bet.Stake?true:false;
+                bet.isHighStake=win.avgStake*10<bet.stake?true:false;
                 //set the flag If customers stake is more than 30 times higher than that customer’s average bet in their betting history
-                bet.isVeryHighStake=win.avgStake*30<bet.Stake?true:false;
+                bet.isVeryHighStake=win.avgStake*30<bet.stake?true:false;
                 }
             }
            }
